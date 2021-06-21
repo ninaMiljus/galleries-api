0df8ae35c8e3a8e508eceeb66df884bcdd0ec19d
ninaMiljus <nina.miljus@gmail.com>
2021-06-21 20:13:32 +0200

    Added index and show methods and routes.

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    //
+    public function index(){
+        $galleries = Gallery::all();
+
+        return response()->json($galleries);
+    }
+
+    public function show($id){
+        $gallery = Gallery::findOrFail($id);
+
+        return response()->json($gallery);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GalleryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('api')->get('/galleries', [GalleryController::class, 'index']);
+Route::middleware('api')->get('/galleries/{id}', [GalleryController::class, 'show']);
+
+
